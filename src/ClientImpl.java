f9213209782613cfaad211349a039c6a3226a5d8
lunuju <lucaslunuju@gmail.com>
2016-11-13 17:53:41 -0200

    lcd fixed

--- src/ClientImpl.java
+++ src/ClientImpl.java
@@ -1,6 +1,7 @@
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -53,97 +54,120 @@ public class ClientImpl {
 				else { System.out.print("Offline @ " + currentPath + "$ "); }
 				
 				if(sd.detectInput() != null) {
-					if(sd.getCommand() == 0) {
-						// client has requested a connection
-						address = new String(sd.getArgument1(), "ASCII");
-						if(greenlight == false) {
-							// only tries to create a connection if there's no connection yet
-							if(connect(address)) {
-								// connection successfully established
-								greenlight = true;
-								outs = new DataOutputStream(socket.getOutputStream());
-								ins = new DataInputStream(socket.getInputStream());
-								
-								// the servers immediately writes its current path
-								
-								int size = ins.readByte();
-								byte[] array = new byte[size];
-								for(int iter = 0; iter < size; iter++) { array[iter] = ins.readByte(); }
-								serverCurrentPath = new String(array, "ASCII");
+					if(sd.getCommand() == 12) {
+						// lcd
+						String directory = new String(sd.getArgument1(), "ASCII");
+						
+						if(directory.equals("..")) {
+							File dir = new File(currentPath);
+							String temp = dir.getParent();
+							
+							if(temp != null) {
+								currentPath = temp;
 							}
-						} else { System.out.println("You are already connected to a server."); }
-					} else if(greenlight) {
-						// client is connected and has inserted a command
-						if(sd.getCommand() == 8) {
-							// client desires to end the connection
-							greenlight = false;
-							outs.close();
-							ins.close();
-							socket.close();
 						} else {
-							// another command was received
-							// the package is only built if there is an active connection
-							// otherwise we'll get an error regarding the socket
+							File dir = new File(currentPath + "/" + directory);
 							
-							outs.write(buildOutput(sd));
-							// a single call of the write function is enough
-							// because a byte array containing every field is given
-							
-							// the first byte always refers to the operation status
-							if(ins.readByte() == 0) {
-								// zero indicates an error
-								int num = ins.readByte();
-								byte[] array = new byte[num];
-								for(int iter = 0; iter < num; iter++) { array[iter] = ins.readByte(); }
-								String string = new String(array, "ASCII");
-								System.out.println(string);
+							if(dir.isDirectory() == true) {
+								currentPath = currentPath + "/" + directory;
 							} else {
-								// otherwise, the operation was a success
-								if(sd.getCommand() == 1) {
-									// ls
-									int num = ins.readByte();
-									for(int iter = 0; iter < num; iter++) {
-										int size = ins.readByte();
-										byte[] array = new byte[size];
-										
-										for(int ite = 0; ite < size; ite++) { array[ite] = ins.readByte(); }
-										
-										String string = new String(array, "ASCII");
-										System.out.println(string);
-									}
-								} else if(sd.getCommand() == 2) {
-									// cd
+								System.out.println(directory + " : no such file or directory.");
+							}
+						}
+						
+					} else {
+						if(sd.getCommand() == 0) {
+							// client has requested a connection
+							address = new String(sd.getArgument1(), "ASCII");
+							if(greenlight == false) {
+								// only tries to create a connection if there's no connection yet
+								if(connect(address)) {
+									// connection successfully established
+									greenlight = true;
+									outs = new DataOutputStream(socket.getOutputStream());
+									ins = new DataInputStream(socket.getInputStream());
+									
+									// the servers immediately writes its current path
+									
 									int size = ins.readByte();
 									byte[] array = new byte[size];
 									for(int iter = 0; iter < size; iter++) { array[iter] = ins.readByte(); }
 									serverCurrentPath = new String(array, "ASCII");
-								} else if(sd.getCommand() == 11) {
-									// download
-									String name = new String(sd.getArgument1(), "ASCII");
-									
-									byte filesizesize = ins.readByte();
-									byte[] filesize = new byte[filesizesize];
-									for(int iter = 0; iter < filesizesize; iter++) { filesize[iter] = ins.readByte(); }
-									String temp = new String(filesize,"ASCII");
-									int realfilesize = Integer.parseInt(temp);
-									byte[] file = new byte[realfilesize];
-									for(int iter = 0; iter < realfilesize; iter++) { file[iter] = ins.readByte(); }
-									
-									
-									FileOutputStream fos = new FileOutputStream(currentPath + "/" + name);
-									fos.write(file);
-									fos.close();
-								} else if(sd.getCommand() == 9) {
-									// cat
-									int size = ins.readByte();
-									byte[] array = new byte[size];
-									for(int iter = 0; iter < size; iter++) { array[iter] = ins.readByte(); }
-									String content = new String(array, "ASCII");
-									System.out.println(content);
-								} else { System.out.println("Operation Stats : Success."); }
+								}
+							} else { System.out.println("You are already connected to a server."); }
+						} else if(greenlight) {
+							// client is connected and has inserted a command
+							if(sd.getCommand() == 8) {
+								// client desires to end the connection
+								greenlight = false;
+								outs.close();
+								ins.close();
+								socket.close();
+							} else {
+								// another command was received
+								// the package is only built if there is an active connection
+								// otherwise we'll get an error regarding the socket
+								
+								outs.write(buildOutput(sd));
+								// a single call of the write function is enough
+								// because a byte array containing every field is given
+								
+								// the first byte always refers to the operation status
+								if(ins.readByte() == 0) {
+									// zero indicates an error
+									int num = ins.readByte();
+									byte[] array = new byte[num];
+									for(int iter = 0; iter < num; iter++) { array[iter] = ins.readByte(); }
+									String string = new String(array, "ASCII");
+									System.out.println(string);
+								} else {
+									// otherwise, the operation was a success
+									if(sd.getCommand() == 1) {
+										// ls
+										int num = ins.readByte();
+										for(int iter = 0; iter < num; iter++) {
+											int size = ins.readByte();
+											byte[] array = new byte[size];
+											
+											for(int ite = 0; ite < size; ite++) { array[ite] = ins.readByte(); }
+											
+											String string = new String(array, "ASCII");
+											System.out.println(string);
+										}
+									} else if(sd.getCommand() == 2) {
+										// cd
+										int size = ins.readByte();
+										byte[] array = new byte[size];
+										for(int iter = 0; iter < size; iter++) { array[iter] = ins.readByte(); }
+										serverCurrentPath = new String(array, "ASCII");
+									} else if(sd.getCommand() == 11) {
+										// download
+										String name = new String(sd.getArgument1(), "ASCII");
+										
+										byte filesizesize = ins.readByte();
+										byte[] filesize = new byte[filesizesize];
+										for(int iter = 0; iter < filesizesize; iter++) { filesize[iter] = ins.readByte(); }
+										String temp = new String(filesize,"ASCII");
+										int realfilesize = Integer.parseInt(temp);
+										byte[] file = new byte[realfilesize];
+										for(int iter = 0; iter < realfilesize; iter++) { file[iter] = ins.readByte(); }
+										
+										
+										FileOutputStream fos = new FileOutputStream(currentPath + "/" + name);
+										fos.write(file);
+										fos.close();
+									} else if(sd.getCommand() == 9) {
+										// cat
+										int size = ins.readByte();
+										byte[] array = new byte[size];
+										for(int iter = 0; iter < size; iter++) { array[iter] = ins.readByte(); }
+										String content = new String(array, "ASCII");
+										System.out.println(content);
+									} else { System.out.println("Operation Stats : Success."); }
+								}
 							}
-						}
-					} else { System.out.println("This command requires online connection."); }
+						} else { System.out.println("This command requires online connection."); }
+					}
 				}
 			}
 		} catch (Exception e) {
